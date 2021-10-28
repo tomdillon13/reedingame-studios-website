29a29f7a82517fdfbf35558a711e21205b8a937e
Tom Dillon <38924622+tomdillon13@users.noreply.github.com>
2021-10-28 17:15:57 +0100

    Delete Portfolio Website directory

--- Portfolio Website/Scripts/index.js
+++
@@ -1,8 +0,0 @@
-var MOTD = ["Yes, we made that car game", "Best esports racing team in the world", "World's most active studio", "Bike Racing sucks!!"]
-
-function choose(Choices){
-	var index = Math.floor(Math.random() * Choices.length);
-	return Choices[index]
-}
-
-document.getElementById("MOTDT").innerHTML = choose(MOTD)
