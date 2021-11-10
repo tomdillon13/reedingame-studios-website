2a024ac2d4161bbcff33e6d7c0de3d89030c5905
Tom Dillon <38924622+tomdillon13@users.noreply.github.com>
2021-11-10 23:15:42 +0000

    Added MOTD to Index.JS
    Literally just trying to test discord webhooks.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,4 @@
-var MOTD = ["Yes, we made that car game", "Best esports racing team in the world", "World's most active studio", "Bike Racing sucks!!"]
+var MOTD = ["Yes, we made that car game", "Best esports racing team in the world", "World's most active studio", "Bike Racing sucks!!", "This MOTD exists only to test discord webhooks, please look away"]
 
 function choose(Choices){
 	var index = Math.floor(Math.random() * Choices.length);
